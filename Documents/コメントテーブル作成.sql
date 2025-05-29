ebefb3c52aee9c2a4fe141c874385ae3b3712a7e
comsize-Furudate <jio.furudate@comsize.com>
2025-05-29 14:40:30 +0900

    コメントテーブル作成用SQL

---
+++ Documents/コメントテーブル作成.sql
@@ -0,0 +1,12 @@
+USE taskdb;
+
+CREATE TABLE taskdb.t_comment
+(
+	comment_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	task_id INT NOT NULL,
+	user_id VARCHAR(24) NOT NULL,
+	comment VARCHAR(100) NOT NULL,
+	update_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	FOREIGN KEY (task_id) REFERENCES t_task(task_id),
+	FOREIGN KEY (user_id) REFERENCES m_user(user_id)
+);
