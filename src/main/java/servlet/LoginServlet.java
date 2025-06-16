6bac2f8f228d1a157952de1a83b8df328334e9f6
comsize-yamagata <mizukiiii.mamo@gmail.com>
2025-06-16 17:48:18 +0900

    developマージ用

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -2,6 +2,9 @@ package servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,7 +14,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.dao.TaskDAO;
 import model.dao.UserDAO;
+import model.entity.TaskBean;
 import model.entity.UserBean;
 
 /**
@@ -66,40 +71,95 @@ public class LoginServlet extends HttpServlet {
 			RequestDispatcher rd = request.getRequestDispatcher("login-failure.jsp");
 			rd.forward(request, response);
 
-		} else {
+		} 
 
-			try {
+		try {
 
-				UserBean userbean = dao.logincheck(userId);
+			UserBean userbean = dao.logincheck(userId);
 
+			
+
+			if (userbean == null) {
+
+				String error = "ユーザーIDまたはパスワードが正しくありません";
+				request.setAttribute("error", error);
+
+				RequestDispatcher rd = request.getRequestDispatcher("login-failure.jsp");
+				rd.forward(request, response);
+				
+			}else if(!userbean.getPassword().equals(pass)) {
+				
+				
+				String error = "ユーザーIDまたはパスワードが正しくありません";
+				request.setAttribute("error", error);
+
+				RequestDispatcher rd = request.getRequestDispatcher("login-failure.jsp");
+				rd.forward(request, response);
+
+			} else {
+
+				HttpSession session = request.getSession();
+				
 				String name = userbean.getUserName();
+			
+				session.setAttribute("id", userId);
+				session.setAttribute("name", name);
+				session.setAttribute("pass", pass);
 
-				if (userbean.getPassword().equals(pass)) {
-
-					String loginsuccess = "成功";
-
-					HttpSession session = request.getSession();
-					session.setAttribute("loginsuccess", loginsuccess);
-					session.setAttribute("id", userId);
-					session.setAttribute("name", name);
-					session.setAttribute("pass", pass);
-
-					RequestDispatcher rd = request.getRequestDispatcher("menu.jsp");
-					rd.forward(request, response);
-
-				} else {
-					String error = "ユーザーIDまたはパスワードが正しくありません";
-					request.setAttribute("error", error);
-
-					RequestDispatcher rd = request.getRequestDispatcher("login-failure.jsp");
-					rd.forward(request, response);
-				}
-
-			} catch (ClassNotFoundException | SQLException e) {
-
-				e.printStackTrace();
 			}
+
+			TaskDAO taskdao = new TaskDAO();
+
+			LocalDate today = LocalDate.now();
+			LocalDate todayAdd3 = today.plusDays(3);
+			LocalDate todayAdd2 = today.plusDays(2);
+			LocalDate todayAdd1 = today.plusDays(1);
+
+			List<TaskBean> taskbeanList = (List<TaskBean>) taskdao.limit(userId);
+			
+			String msg = null;
+		
+			HttpSession session = request.getSession();
+			
+			for(TaskBean taskBean : taskbeanList) {
+			
+				if(taskBean != null) {
+				
+					LocalDate limit = ((TaskBean) taskBean).getLimit();
+					
+					if (limit.isAfter(today) && limit.isBefore(todayAdd1)) {
+						
+						msg = "期限が1日以内のタスクがあります";
+						break;
+						
+					}else if(limit.isAfter(today) && limit.isBefore(todayAdd2)){
+						
+						msg = "期限が2日以内のタスクがあります";
+						break;
+						
+					}else if(limit.isAfter(today) && limit.isBefore(todayAdd3)) {
+						
+						msg = "期限が3日以内のタスクがあります";
+						
+						
+					}
+					
+					session.setAttribute("msg", msg);
+					
+				}
+			}
+			
+			session.setAttribute("msg", msg);
+			session.setAttribute("alert", true);	
+			RequestDispatcher rd = request.getRequestDispatcher("menu.jsp");
+			rd.forward(request, response);
+
+		} catch (ClassNotFoundException | SQLException | NullPointerException | ParseException e) {
+
+			e.printStackTrace();
 		}
+
+		
 
 	}
 }
