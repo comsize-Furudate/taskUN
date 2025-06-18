e9d47abb58132c07e242b2f0afc5c22b1c054b50
comsize-AZUSA <azusa.yamamoto@comsize.com>
2025-06-18 15:31:50 +0900

    設計書用CommentDAO,CommentBean

---
+++ src/main/java/model/dao/CommentDAO.java
@@ -0,0 +1,21 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entity.CommentBean;
+
+public class CommentDAO {
+	
+	public List<CommentBean> selectComment(int taskId){
+		return null;
+	}
+	
+	public int insertComment(CommentBean bean) {
+		return 0;
+	}
+	
+	public int deleteComment(int commentId) {
+		return 0;
+	}
+
+}
